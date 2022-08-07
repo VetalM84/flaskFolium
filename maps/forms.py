b6a87039307a20c91bd3fee732a863eb98f8b234
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-08 00:37:56 +0300

    Removed comment field. Edit placeholder.

--- maps/forms.py
+++ maps/forms.py
@@ -1,32 +1,29 @@
 """Form storage for the map module."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class LocationForm(FlaskForm):
+    """Form for adding new marker to the map."""
     coordinates = StringField(
         "Координаты",
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Формат: 49.988, -36.2334"},
+        validators=[DataRequired(), Length(min=7, max=100)],
+        render_kw={"placeholder": "Формат: 49.9881, 22.2334"},
     )
-    comment = TextAreaField(
-        "Комментарий",
-        validators=[Length(min=0, max=50)],
-        render_kw={"rows": "2", "placeholder": "Важная информация, если есть"},
-    )
+    # comment = TextAreaField(
+    #     "Комментарий",
+    #     validators=[Length(min=0, max=50)],
+    #     render_kw={"rows": "2", "placeholder": "Важная информация, если есть"},
+    # )
     color = SelectField(
         "Событие", choices=[("red", "Вручают"), ("green", "Никого нет")]
     )
     submit = SubmitField("Добавить")
 
     # def validate_coordinates(self, coordinates):
-    #     title = Book.query.filter_by(title=title.data).first()
-    #     if title:
-    #         raise ValidationError('Такая книга уже есть в списке прочитанных.')
-
-    # def validate_comment(self, comment):
-    #     title = Book.query.filter_by(title=title.data).first()
-    #     if title:
-    #         raise ValidationError('Такая книга уже есть в списке прочитанных.')
+    #     # title = Book.query.filter_by(title=title.data).first()
+    #     print(coordinates.data)
+    #     if str(coordinates.data).find(",,") == -1:
+    #         raise ValidationError('Ошибка. Неудалось распознать координаты.')
