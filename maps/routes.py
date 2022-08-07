b4b849564abd0ad4385027c1814d90fc8ef92b44
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-07 15:11:08 +0300

    Code refactor.

--- maps/routes.py
+++ maps/routes.py
@@ -9,42 +9,51 @@ from maps import app
 from maps.forms import LocationForm
 
 
-@app.route('/about/')
+@app.route("/about/")
 def about():
     """About page."""
-    return render_template('about.html')
+    return render_template("about.html")
 
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route("/", methods=("GET", "POST"))
 def index():
     """Index page. Showing map with markers and form to add new marker."""
     form = LocationForm()
     start_location = (49.98841410732452, 36.23353094883754)
-    m = folium.Map(location=start_location, zoom_start=12)
-    Fullscreen(position="topright", title="Полный экран", title_cancel="Выход").add_to(m)
-    LatLngPopup().add_to(m)
-    LocateControl(auto_start=False, position="topright", strings={"title": "Где я"}).add_to(m)
+    current_map = folium.Map(location=start_location, zoom_start=12)
+
+    # Map control buttons and plugins
+    Fullscreen(position="topright", title="Полный экран", title_cancel="Выход").add_to(
+        current_map
+    )
+    LocateControl(
+        auto_start=False, position="topright", strings={"title": "Где я"}
+    ).add_to(current_map)
+    LatLngPopup().add_to(current_map)
 
     if form.validate_on_submit():
-        location = form.coordinates.data.split(',')
+        location = form.coordinates.data.split(",")
         color = form.color.data
         comment = form.comment.data
 
         add_marker(
-            m=m,
-            location=(float(location[0]), float(location[1])),  # location=[45.5, -122.3]
+            current_map=current_map,
+            location=(
+                float(location[0]),
+                float(location[1]),
+            ),  # location=[45.5, -122.3]
             color=color,
             popup=comment,
         )
 
-    return render_template('index.html', form=form, maps=m._repr_html_())
+    return render_template("index.html", form=form, maps=current_map._repr_html_())
 
 
-def add_marker(m, location, color, popup):
+def add_marker(current_map, location, color, popup):
     """Add marker to map."""
     folium.Marker(
         location=location,
         icon=folium.Icon(color=color, icon="info-sign"),
         popup=popup,
         tooltip=popup,
-    ).add_to(m)
+    ).add_to(current_map)
