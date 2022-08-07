a989546bb656acab06492d72f74e3e675b2485ed
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-07 15:10:32 +0300

    Add placeholders

--- maps/forms.py
+++ maps/forms.py
@@ -1,19 +1,24 @@
 """Form storage for the map module."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
-from flask_wtf.file import FileField, FileAllowed
-from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
+from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class LocationForm(FlaskForm):
     coordinates = StringField(
         "Координаты",
         validators=[DataRequired()],
-        description="Формат: 49.988, -36.2334",
+        render_kw={"placeholder": "Формат: 49.988, -36.2334"},
     )
-    comment = StringField("Комментарий", validators=[Length(min=0, max=50)])
-    color = SelectField("Событие", choices=[("red", "Вручают"), ("green", "Никого нет")])
+    comment = TextAreaField(
+        "Комментарий",
+        validators=[Length(min=0, max=50)],
+        render_kw={"rows": "2", "placeholder": "Важная информация, если есть"},
+    )
+    color = SelectField(
+        "Событие", choices=[("red", "Вручают"), ("green", "Никого нет")]
+    )
     submit = SubmitField("Добавить")
 
     # def validate_coordinates(self, coordinates):
