a08c854fdf86522978126f05cd1f1adad8f265a6
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-21 11:55:22 +0300

    Rename Marker to Report. Add docstring.

--- maps/api/v1/endpoints.py
+++ maps/api/v1/endpoints.py
@@ -1,4 +1,5 @@
-"""API endpoints for the maps app."""
+"""API endpoints for the maps' app."""
+
 from datetime import datetime
 
 from flask import request, jsonify
@@ -8,28 +9,18 @@ from maps import apis
 from maps.routes import get_all_markers, to_date
 
 
-class Markers(Resource):
+class Report(Resource):
+    """Report resource from Report DB model."""
 
     def get(self):
-        #     """
-        #     Retrieve all markers with date provided. If not date provided, return all markers by today.
-        #     Use 2022-08-14 arg date format to get all markers from 14 August 2022.
-        #     """
+        """
+        Retrieve all markers with date provided. If not date provided, return all markers by today.
+        Use 2022-08-14 arg date format to get all markers from 14 August 2022.
+        """
         date_filter_argument = request.args.get(
             "date", default=datetime.today().date(), type=to_date
         )
         return jsonify(get_all_markers(date=date_filter_argument))
 
 
-apis.add_resource(Markers, '/api/v1/markers/')
-
-# @app.route("/api/v1/markers/", methods=["GET"])
-# def get_markers():
-#     """
-#     Retrieve all markers with date provided. If not date provided, return all markers by today.
-#     Use 2022-08-14 arg date format to get all markers from 14 August 2022.
-#     """
-#     date_filter_argument = request.args.get(
-#         "date", default=datetime.today().date(), type=to_date
-#     )
-#     return jsonify(get_all_markers(date=date_filter_argument))
+apis.add_resource(Report, "/api/v1/markers/")
