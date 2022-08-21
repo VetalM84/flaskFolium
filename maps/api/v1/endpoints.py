98ad7b43ecc1278cc683e7554d86fc9ef0853697
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-21 21:55:03 +0300

    Add PUT method for adding Report to DB

--- maps/api/v1/endpoints.py
+++ maps/api/v1/endpoints.py
@@ -1,14 +1,12 @@
 """API endpoints for the maps' app."""
 
-from datetime import datetime
-
 from flask import request
 from flask_restx import Resource, fields, marshal_with
 
 from maps import apis
-from maps.routes import get_all_markers, to_date
+from maps.routes import get_all_markers, to_date, add_report_to_db
 
-report_model = apis.model(
+get_model = apis.model(
     "Report",
     {
         "id": fields.Integer,
@@ -20,18 +18,43 @@ report_model = apis.model(
     },
 )
 
+post_model = apis.model(
+    "Report",
+    {
+        "latitude": fields.Float,
+        "longitude": fields.Float,
+        "color": fields.String,
+        "comment": fields.String(required=False),
+    },
+)
 
-@apis.route("/api/v1/markers/")
+
+@apis.route("/api/v1/markers/<string:date>/", methods=["GET"])
 class Report(Resource):
     """Report resource from Report DB model."""
 
-    @marshal_with(report_model)
-    def get(self):
+    @marshal_with(get_model)
+    def get(self, date):
         """
-        Retrieve all markers with date provided. If not date provided, return all markers by today.
+        Retrieve all markers with date provided.
         Use 2022-08-14 arg date format to get all markers from 14 August 2022.
         """
-        date_filter_argument = request.args.get(
-            "date", default=datetime.today().date(), type=to_date
+        return get_all_markers(request_date=to_date(date))
+
+
+@apis.route("/api/v1/markers/", methods=["POST", "PUT"])
+class AddReport(Resource):
+    """Add Report resource to DB model."""
+
+    @apis.doc(responses={201: "Created", 400: "Validation error"})
+    @apis.expect(post_model)
+    @marshal_with(post_model)
+    def put(self):
+        """POST or PUT method for adding marker to DB."""
+        add_report_to_db(
+            latitude=request.json["latitude"],
+            longitude=request.json["longitude"],
+            color=request.json["color"],
+            comment=request.json["comment"],
         )
-        return get_all_markers(request_date=date_filter_argument)
+        return request.json, 201
