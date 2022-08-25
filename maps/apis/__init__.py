2255e21881e3ae8e6dc155501e1d184de17a2d64
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-25 20:48:49 +0300

    Bug fix link to Swagger

--- maps/apis/__init__.py
+++ maps/apis/__init__.py
@@ -6,7 +6,7 @@ from flask_restx import Api
 blueprint = Blueprint('api', __name__)
 api = Api(
     app=blueprint,
-    doc="/api/v1/doc/",
+    doc="/doc/",
     version="1.0",
     title="Maps API",
     description="Maps API",
