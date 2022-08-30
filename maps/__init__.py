80db1cbe75978e44c83d415bd4c6c84864c2e0e5
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-30 13:46:43 +0300

    Add alembic Migrate

--- maps/__init__.py
+++ maps/__init__.py
@@ -4,11 +4,13 @@ import logging
 
 from flask import Flask
 from flask_caching import Cache
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
 db = SQLAlchemy()
+migrate = Migrate()
 cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
 
 
@@ -22,6 +24,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     db.init_app(app)
+    migrate.init_app(app, db)
     cache.init_app(app)
 
     from maps import routes
