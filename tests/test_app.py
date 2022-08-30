ae34e5a591b8ed510399de24ee157847c24c1a17
Vitalii Mytenko <vetalmit@gmail.com>
2022-08-30 11:32:37 +0300

    Add tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,32 @@
 """Test cases for frontend."""
 
-import pytest
+import unittest
 
-from maps import app
+from flask import current_app
 
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
+from maps import create_app, db
 
 
-class TestApp:
+class BasicsTestCase(unittest.TestCase):
+    """General test cases."""
 
     def setUp(self):
-        pass
+        """Execute before unit test. Create an app and in memory DB."""
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
 
-    def test_index(self):
-        response = app.test_client().get('/')
-        assert response.status_code == 200
+    def tearDown(self):
+        """Execute after unit test. Drop DB, session, app."""
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
-    def test_about(self):
-        response = app.test_client().get("/about/")
-        assert response.status_code == 200
-        # assert template.name == "base.html"
-        # assert response.data.decode('utf-8') == 'Testing, Flask!'
+    def test_app_exists(self):
+        """Check if it is possible to create an app instance."""
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        """Check if environment is set to TESTING == True."""
+        self.assertTrue(current_app.config["TESTING"])
